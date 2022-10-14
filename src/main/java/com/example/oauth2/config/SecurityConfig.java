05f3a9f8a61728129dda99ff3bb94030d2efa90a
arifrakhmadi <job.arif86@gmail.com>
2022-10-14 16:21:04 +0700

    membuat login default dan permission

---
+++ src/main/java/com/example/oauth2/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.example.oauth2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    private static final String SQL_LOGIN
+            = "select u.username, up.password, u.active " +
+            "from s_users_passwords up " +
+            "inner join s_users u on u.id = up.id_user " +
+            "where u.username = ?";
+
+    private static final String SQL_PERMISSION =
+            "select u.username, p.value as authority " +
+                    "from s_users u " +
+                    "inner join s_roles r on r.id = u.id_role " +
+                    "inner join s_roles_permissions rp on rp.id_role = r.id " +
+                    "inner join s_permissions p on rp.id_permission = p.id " +
+                    "where u.username = ?";
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> {
+
+                    auth.anyRequest().authenticated();
+
+                })
+                .formLogin()
+                .and()
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(this.dataSource);
+        manager.setUsersByUsernameQuery(SQL_LOGIN);
+        manager.setAuthoritiesByUsernameQuery(SQL_PERMISSION);
+        return manager;
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+}
--- src/main/java/com/example/oauth2/controller/home.java
+++ src/main/java/com/example/oauth2/controller/home.java
@@ -1,11 +1,13 @@
 package com.example.oauth2.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class home {
 
+    @PreAuthorize("hasAuthority('EDIT_TRANSAKSI')")
     @GetMapping("/home")
     public void home(){
 
