faf5f0b0c43af662b7eb4b04520ee841a3457935
arifrakhmadi <job.arif86@gmail.com>
2022-10-20 16:28:50 +0700

    menampilkan nilai user login di thymeleaf dan mencoba oauth2

---
+++ README.md
@@ -0,0 +1,9 @@
+authorization_endpoint :
+http://localhost:8080/oauth2/authorize?client_id=mobileapp&redirect_uri=http://example.com&response_type=code&scope=openid&state=abcd1234&code_challenge_method=S256&code_challenge=51299d8509a092f1d7870e3c470c4b668e8f7d26e6f132a81b97e2f00cd5a8eb
+
+client id : mobileapp
+client_secret : mobileapp123456
+hash client secret : $2a$10$fqkks2.X4zszMxI5P2zrNuSnhVaDGoWn4dSS4QyvElNxomnJwZ2/m
+
+CODE VERIFIER : C1U3COCC1YOQ3O07G2ZA
+CODE CHALLENG : 51299d8509a092f1d7870e3c470c4b668e8f7d26e6f132a81b97e2f00cd5a8eb 
--- src/main/java/com/example/oauth2/controller/HomeController.java
+++ src/main/java/com/example/oauth2/controller/HomeController.java
@@ -1,15 +1,21 @@
 package com.example.oauth2.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
-    @PreAuthorize("hasAuthority('EDIT_TRANSAKSI')")
+    @PreAuthorize("hasAnyAuthority('VIEW_TRANSAKSI','EDIT_TRANSAKSI')")
     @GetMapping("/home")
-    public void home(){
+    public void home(Authentication authentication){
+
+        log.info("jenis class : {}", authentication.getClass());
+        log.info("user yang sedang login : {}", authentication.getPrincipal());
 
     }
 
